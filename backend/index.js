148ec8fa837f09b88bbd12312bb4298038e09f62
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-12 21:46:49 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://tradition-tales-blog-front.vercel.app/login"],
     methods: ["POST" , "GET"],
     credentials: true
   }
