df15da2a3c2c0a818a960cdb8edb0dbbd217053b
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-12 19:19:51 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,20 @@ dotenv.config();
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["https://Tradition-Tales-Blog.vercel.app"],
+    methods: ["POST" , "GET"],
+    credentials: true
+  }
+));
+
+mongoose.connect('mongodb+srv://aditi:1234ad@merncluster.5km0ode.mongodb.net/test?retryWrites=true&w=majority&appName=AtlasApp')
+
 app.use(bodyParser.json({ extended: true}))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use('/', Router);
+
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`))
