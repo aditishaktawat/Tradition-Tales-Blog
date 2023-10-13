36569a88ed385de02c1a2ed7851d226cd52cd482
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-13 10:22:19 +0530

    db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
  const Connection = async(username,password) => {
-    const URL = `mongodb+srv://${username}:${password}@merncluster.5km0ode.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+   
     try{
      
         await mongoose.connect(URL,{useNewUrlParser: true});
