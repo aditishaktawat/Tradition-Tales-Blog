10b7297d7c69094c5ab4f690adaa0fb1356361ee
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-13 13:35:04 +0530

    update.jsx

--- client/src/components/create/update.jsx
+++ client/src/components/create/update.jsx
@@ -1,140 +1,136 @@
-import {useState ,useEffect, useContext } from 'react';
-
-import {Box, styled ,FormControl, InputBase, Button, TextareaAutosize} from '@mui/material';
-import {AddCircle as Add} from '@mui/icons-material';
-
-import { useLocation , useNavigate, useParams} from 'react-router-dom';
-
+import { useState, useEffect, useContext } from 'react';
+import { Box, styled, FormControl, InputBase, Button, TextareaAutosize } from '@mui/material';
+import { AddCircle as Add } from '@mui/icons-material';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { DataContext } from '../../context/DataProvider';
-
-import {API} from '../../service/api'
-
+import { API } from '../../service/api';
 
 const Container = styled(Box)(({ theme }) => ({
-   margin: '50px 100px',
-   [theme.breakpoints.down('md')]: {
-      margin: 0
-   }
+  margin: '50px 100px',
+  [theme.breakpoints.down('md')]: {
+    margin: 0,
+  },
 }));
 
 const Image = styled('img')({
-    width: '100%',
-    height: '50vh',
-    objectFit: 'cover'
-
+  width: '100%',
+  height: '50vh',
+  objectFit: 'cover',
 });
 
 const StyledFormContainer = styled(FormControl)`
-   margin-top: 10px;
-   display: flex;
-   flex-direction: row;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
 `;
 
 const InputTextField = styled(InputBase)`
-    flex: 1;
-    margin: 0 30px;
-    font-size: 25px;
-
+  flex: 1;
+  margin: 0 30px;
+  font-size: 25px;
 `;
 
-const TextArea = styled (TextareaAutosize)`
-   width: 100%;
-   margin-top: 50px;
-   font-size: 18px;
-
+const TextArea = styled(TextareaAutosize)`
+  width: 100%;
+  margin-top: 50px;
+  font-size: 18px;
 `;
 
 const initialPost = {
-   title: '',
-   description: '',
-   picture: '',
-   username: '',
-   categories: '',
-   createdDate: new Date()
-}
-
-const Update =() => {
-
-
-const [post, setPost] = useState(initialPost);
-const[file, setFile] = useState('');
-
-const {account} = useContext(DataContext);
-
-const location = useLocation();
-const navigate = useNavigate();
-const { id } = useParams();
-
-const url = post.picture ? post.picture: 'https://images.freecreatives.com/wp-content/uploads/2015/05/vintage-photography-backgrounds.jpg';
-
-useEffect(() => {
-   const fetchData = async () => {
-        let res = await API.getPostById(id);
-        if(res.isSuccess) {
-           setPost(res.data);
-        }
-   }
-   fetchData();
-}, [id]);
-
-useEffect(() => {
-   const getImage = async() => {
-      if (file) {
-         const data = new FormData();
-         data.append("name", file.name);
-         data.append("file", file);
-
-         // Api call
-         const res = await API.uploadFile(data);
-         post.picture = res.data ;
-      }
-   }
-   getImage();
-    post.categories = location.search?.split('=')[1] || 'All';
-    post.username = account.username;
-}, [file, account.username, location.search, post]);
-
-
-const handleChange = (e) => {
-   setPost({ ...post, [e.target.name]: e.target.value})
-}
-
-const updateBlogPost = async() => {
-  let res =  await API.updatePost(post);
-  if(res.isSuccess) {
-   navigate(`/details/${id}`);
-  }
+  title: '',
+  description: '',
+  picture: '',
+  username: '',
+  categories: '',
+  createdDate: new Date(),
 };
 
-    return (
-       <Container>
-        <Image src={url} alt="banner" />
+const Update = () => {
+  const [post, setPost] = useState(initialPost);
+  const [file, setFile] = useState(null); // File state should start as null
+  const [imageDataURL, setImageDataURL] = useState('');
+  const { account } = useContext(DataContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-        <StyledFormContainer>
-           <label htmlFor="fileInput">
-             <Add  fontSize='large' color="action"/>
-           </label>
-           <input 
-            type="file"
-            id="fileInput"
-            style= {{ display: 'none'}}
-            onChange={(e) => setFile(e.target.files[0])}
-           />
+  useEffect(() => {
+    const fetchData = async () => {
+      let res = await API.getPostById(id);
+      if (res.isSuccess) {
+        setPost(res.data);
+      }
+    };
+    fetchData();
+  }, [id]);
 
-         <InputTextField placeholder='Title' value={post.title} onChange={(e) => handleChange(e)} name="title" />
-         <Button variant="contained" onClick={() => updateBlogPost()}>Update</Button>
-        </StyledFormContainer>
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setImageDataURL(event.target.result);
+      };
+      reader.readAsDataURL(selectedFile);
+      setFile(selectedFile);
+    }
+  };
 
-        <TextArea
-            minRows={5}
-            placeholder="Share your experience...."
-            onChange={(e) => handleChange(e)} 
-            name="description"
-            value={post.description}
+  const handleChange = (e) => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
+
+  const updateBlogPost = async () => {
+    if (file) {
+      const data = new FormData();
+      data.append('name', file.name);
+      data.append('file', file);
+      const res = await API.uploadFile(data);
+      post.picture = res.data;
+    }
+    post.categories = location.search?.split('=')[1] || 'All';
+    post.username = account.username;
+
+    let res = await API.updatePost(post);
+    if (res.isSuccess) {
+      navigate(`/details/${id}`);
+    }
+  };
+
+  return (
+    <Container>
+      <Image src={imageDataURL || post.picture || 'https://images.freecreatives.com/wp-content/uploads/2015/05/vintage-photography-backgrounds.jpg'} alt="banner" />
+
+      <StyledFormContainer>
+        <label htmlFor="fileInput">
+          <Add fontSize="large" color="action" />
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: 'none' }}
+          onChange={(e) => handleFileChange(e)}
         />
 
-       </Container>
-    );
+        <InputTextField placeholder="Title" value={post.title} onChange={(e) => handleChange(e)} name="title" />
+        <Button
+          variant="contained"
+          onClick={() => updateBlogPost()}
+          disabled={!file} // Disable the button if no file is selected
+        >
+          Update
+        </Button>
+      </StyledFormContainer>
+
+      <TextArea
+        minRows={5}
+        placeholder="Share your experience...."
+        onChange={(e) => handleChange(e)}
+        name="description"
+        value={post.description}
+      />
+    </Container>
+  );
 };
 
 export default Update;
