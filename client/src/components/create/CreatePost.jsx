c6ca64eb2e0a141d491d67074066b4b399170981
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-13 12:18:22 +0530

    CreatePost.jsx

--- client/src/components/create/CreatePost.jsx
+++ client/src/components/create/CreatePost.jsx
@@ -59,12 +59,13 @@ const CreatePost =() => {
 const [post, setPost] = useState(initialPost);
 const[file, setFile] = useState('');
 
+const [imageDataURL, setImageDataURL] = useState('');
+
 const {account} = useContext(DataContext);
 
 const location = useLocation();
 const navigate = useNavigate();
 
-const url = post.picture ? post.picture : 'https://images.freecreatives.com/wp-content/uploads/2015/05/vintage-photography-backgrounds.jpg';
 
 useEffect(() => {
    const getImage = async() => {
@@ -84,6 +85,19 @@ useEffect(() => {
 }, [file, account.username, location.search, post]);
 
 
+const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        if (selectedFile) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                setImageDataURL(event.target.result);
+            };
+            reader.readAsDataURL(selectedFile);
+            setFile(selectedFile);
+        }
+    };
+
+
 const handleChange = (e) => {
    setPost({ ...post, [e.target.name]: e.target.value})
 }
@@ -97,7 +111,7 @@ const savePost = async() => {
 
     return (
        <Container>
-        <Image src={url} alt="banner" />
+         <Image src={imageDataURL || post.picture || 'https://images.freecreatives.com/wp-content/uploads/2015/05/vintage-photography-backgrounds.jpg'} alt="banner" />
 
         <StyledFormContainer>
            <label htmlFor="fileInput">
