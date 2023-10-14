ea5ee37c31fa8b601fa5e5fd8e1d1feb3a18d558
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-14 13:21:08 +0530

    upload.js

--- backend/utils/upload.js
+++ backend/utils/upload.js
@@ -7,14 +7,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const username= process.env.DB_USERNAME;
-const password= process.env.DB_PASSWORD;
+const USERNAME= process.env.DB_USERNAME;
+const PASSWORD= process.env.DB_PASSWORD;
+const URL = process.env.MONGODB_URI || `mongodb+srv://${USERNAME}:${PASSWORD}@merncluster.5km0ode.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+
 
 const storage = new GridFsStorage({
-    url:`mongodb+srv://${username}:${password}@merncluster.5km0ode.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`,
+    url: URL,
     options: { useNewUrlParser: true},
     file: (req, file ) => {
-        const match = ["image/png", "image/jpg"];
+        const match = ["image/png", "image/jpg" , "image/jpeg", "image/gif"];
 
         if (match.indexOf(file.mimetype) === -1){
             return{
